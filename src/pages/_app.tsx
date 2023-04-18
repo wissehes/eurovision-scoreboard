afe8da1be3dd3507cf58df1efae8ff893c5c4427
Wisse <wissehes@gmail.com>
2023-04-18 20:39:02 +0200

    Add mantine stuff

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,14 +6,36 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
+
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+
+      <SessionProvider session={session}>
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            /** Put your mantine theme override here */
+            colorScheme: "light",
+          }}
+        >
+          <Component {...pageProps} />
+        </MantineProvider>
+      </SessionProvider>
+    </>
   );
 };
 
