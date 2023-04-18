18b04e8886fb41f69af194c3a4ccfe00bd031b1b
Wisse <wissehes@gmail.com>
2023-04-18 21:27:07 +0200

    Create LinkBreadcrumbs

---
+++ src/components/LinkBreadcrumbs.tsx
@@ -0,0 +1,26 @@
+import { Anchor, Breadcrumbs, type BreadcrumbsProps } from "@mantine/core";
+import Link from "next/link";
+
+interface Link {
+  label: string;
+  href: string;
+}
+
+interface LinkBreadcrumbProps extends Omit<BreadcrumbsProps, "children"> {
+  links: Link[];
+}
+
+export default function LinkBreadcrumbs({
+  links,
+  ...props
+}: LinkBreadcrumbProps) {
+  return (
+    <Breadcrumbs {...props}>
+      {links.map((l) => (
+        <Anchor key={l.href} href={l.href} component={Link}>
+          {l.label}
+        </Anchor>
+      ))}
+    </Breadcrumbs>
+  );
+}
