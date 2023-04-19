26332dbd5334c8385e831b5b8bfebf5c85c30879
Wisse <wissehes@gmail.com>
2023-04-19 16:34:07 +0200

    Create CountryRouter

--- src/server/api/routers/countryRouter.ts
+++ src/server/api/routers/countryRouter.ts
@@ -1,13 +1,67 @@
-// import { z } from "zod";
+import { z } from "zod";
 
 import {
   createTRPCRouter,
   publicProcedure,
-  //   protectedProcedure,
+  // protectedProcedure,
+  adminProcedure,
 } from "~/server/api/trpc";
+
+import { parse as parseCSV } from "csv-string";
+import { type Comma } from "csv-string/dist/types";
+import delay from "~/utils/delay";
 
 export const countryRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.country.findMany();
+    return ctx.prisma.country.findMany({
+      include: { _count: { select: { items: true } } },
+      orderBy: { id: "asc" },
+    });
   }),
+
+  create: adminProcedure
+    .input(z.object({ abbreviation: z.string(), fullname: z.string().min(3) }))
+    .mutation(({ ctx, input: { abbreviation, fullname } }) => {
+      return ctx.prisma.country.create({
+        data: { id: abbreviation, fullname },
+      });
+    }),
+
+  createFromCSV: adminProcedure
+    .input(
+      z.object({
+        csvText: z.string(),
+        delimiter: z.string().nonempty().default(","),
+        idColName: z.string().default("id"),
+        fullnameColName: z.string().default("fullname"),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const data = parseCSV(input.csvText, {
+        comma: input.delimiter as Comma,
+        output: "objects",
+      });
+      const idCol = input.idColName;
+      const fullnameCol = input.fullnameColName;
+
+      const schema = z.array(
+        z.object({ id: z.string(), fullname: z.string() })
+      );
+
+      const mapped = data
+        .map((v) => ({
+          id: v[idCol],
+          fullname: v[fullnameCol],
+        }))
+        .filter((v) => v.id?.trim().length && v.fullname?.trim().length);
+
+      const verified = schema.parse(mapped);
+      console.log(verified);
+
+      await delay(1500);
+
+      await ctx.prisma.country.createMany({ data: verified });
+
+      return mapped;
+    }),
 });
