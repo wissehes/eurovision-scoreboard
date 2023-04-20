06ec5558acd08c9ea2fef78367c8739bc2104409
Wisse <wissehes@gmail.com>
2023-04-20 15:04:34 +0200

    Add create procedure

--- src/server/api/routers/yearRouter.ts
+++ src/server/api/routers/yearRouter.ts
@@ -1,20 +1,14 @@
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import {
+  adminProcedure,
   createTRPCRouter,
   publicProcedure,
   //   protectedProcedure,
 } from "~/server/api/trpc";
 
 export const yearRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
-
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.eurovisionYear.findMany({ include: { items: true } });
   }),
@@ -24,7 +18,30 @@ export const yearRouter = createTRPCRouter({
     .query(({ input, ctx }) => {
       return ctx.prisma.eurovisionYear.findUnique({
         where: { year: input.year },
-        include: { items: { include: { items: true } } },
+        include: {
+          items: { include: { items: true }, orderBy: { type: "asc" } },
+        },
+      });
+    }),
+
+  create: adminProcedure
+    .input(z.object({ year: z.number().min(1950) }))
+    .mutation(async ({ ctx, input }) => {
+      const exists = await ctx.prisma.eurovisionYear.findFirst({
+        where: { year: input.year },
+      });
+
+      if (exists) {
+        throw new TRPCError({
+          code: "CONFLICT",
+          message: "That year already exists.",
+        });
+      }
+
+      return ctx.prisma.eurovisionYear.create({
+        data: {
+          year: input.year,
+        },
       });
     }),
 });
