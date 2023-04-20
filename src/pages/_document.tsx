3e948cd0248b1e3a7835b4c228e116603bc930bc
Wisse <wissehes@gmail.com>
2023-04-20 15:25:33 +0200

    Add react dnd stuff

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,23 @@
 import { createGetInitialProps } from "@mantine/next";
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import Document, {
+  type DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+import { resetServerContext } from "react-beautiful-dnd";
 
 const getInitialProps = createGetInitialProps();
 
 export default class _Document extends Document {
-  static getInitialProps = getInitialProps;
+  static async getInitialProps(ctx: DocumentContext) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const initialProps = await getInitialProps(ctx);
+    resetServerContext();
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return { ...initialProps };
+  }
 
   render() {
     return (
