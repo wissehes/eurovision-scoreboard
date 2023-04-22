7d2a6f7c7c98ecaa571475708a16c33a1eb916ee
Wisse <wissehes@gmail.com>
2023-04-22 16:57:26 +0200

    Add a little loading state

--- src/components/Auth/SignIn/SignInButton.tsx
+++ src/components/Auth/SignIn/SignInButton.tsx
@@ -1,15 +1,23 @@
 import { Button } from "@mantine/core";
 import { signIn } from "next-auth/react";
+import { useState } from "react";
 import { type Provider, colors, icons } from "~/utils/auth/authPage";
 
 export default function SignInButton({ provider }: { provider: Provider }) {
+  const [loading, setLoading] = useState(false);
   const IconComp = icons[provider.id];
+
+  const onPress = () => {
+    setLoading(true);
+    void signIn(provider.id);
+  };
 
   return (
     <Button
       leftIcon={IconComp ? <IconComp size="1.5rem" /> : undefined}
-      onClick={() => void signIn(provider.id)}
+      onClick={onPress}
       color={colors[provider.id] || undefined}
+      loading={loading}
       size="md"
     >
       {provider.name}
