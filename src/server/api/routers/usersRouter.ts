216717bc5276a12249419b8ad29d3544f86b6e09
Wisse <wissehes@gmail.com>
2023-04-25 21:57:58 +0200

    Update usersRouter

--- src/server/api/routers/usersRouter.ts
+++ src/server/api/routers/usersRouter.ts
@@ -1,8 +1,12 @@
+import { TRPCError } from "@trpc/server";
+import { z } from "zod";
+
 import {
   createTRPCRouter,
   // publicProcedure,
   // protectedProcedure,
   adminProcedure,
+  protectedProcedure,
 } from "~/server/api/trpc";
 
 export const usersRouter = createTRPCRouter({
@@ -11,4 +15,22 @@ export const usersRouter = createTRPCRouter({
       include: { accounts: { select: { provider: true } } },
     });
   }),
+
+  me: protectedProcedure.query(async ({ ctx }) => {
+    const data = await ctx.prisma.user.findUnique({
+      where: { id: ctx.session.user.id },
+      include: { accounts: { select: { provider: true } } },
+    });
+    if (!data) throw new TRPCError({ code: "BAD_REQUEST" });
+    return data;
+  }),
+
+  updateName: protectedProcedure
+    .input(z.object({ name: z.string().min(3).max(32) }))
+    .mutation(async ({ ctx, input }) =>
+      ctx.prisma.user.update({
+        where: { id: ctx.session.user.id },
+        data: { name: input.name },
+      })
+    ),
 });
